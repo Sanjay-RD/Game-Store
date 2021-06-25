e780cdda00a69c0e05b5b83c5b8c0c1a32c2b3f6
Sanjay-RD <rishidevsanjay9@gmai.com>
2021-06-25 22:48:16 +0545

    Django postgress setup & migrate

--- app/gamestore/settings.py
+++ app/gamestore/settings.py
@@ -79,8 +79,11 @@ WSGI_APPLICATION = 'gamestore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gamestore',
+        'USER':'postgres',
+        'PASSWORD':'12345',
+        'HOST':'localhost'
     }
 }
 
