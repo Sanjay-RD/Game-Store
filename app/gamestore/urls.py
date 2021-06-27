f3fc2c257f0c38772a0206ddf5cfc3797692a47a
Sanjay-RD <rishidevsanjay9@gmai.com>
2021-06-27 20:03:39 +0545

    Setting media folder

--- app/gamestore/urls.py
+++ app/gamestore/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('',include('pages.urls')),
-    path('game/',include('games.urls')),
+    path('', include('pages.urls')),
+    path('game/', include('games.urls')),
     path('admin/', admin.site.urls),
-]
+]+static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
