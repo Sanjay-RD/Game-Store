1e5ac26e7c6972c6bbd7cce870a45684b0713ca3
Sanjay-RD <rishidevsanjay9@gmail.com>
2021-07-01 23:26:50 +0545

    Dynamic pagination

--- app/games/views.py
+++ app/games/views.py
@@ -1,12 +1,19 @@
+from django.core import paginator
 from django.shortcuts import render
-from .models import Game 
+from .models import Game
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+
 
 def games(request):
-    games = Game.objects.all()
+    games = Game.objects.order_by('-created_at')
+    paginator = Paginator(games, 3)
+    page = request.GET.get('page')
+    paged_games = paginator.get_page(page)
     context = {
-        'games':games
+        'games': paged_games
     }
-    return render(request,'games/games.html',context)
+    return render(request, 'games/games.html', context)
 
-def game(request,game_id):
-    return render(request,'games/game.html')
+
+def game(request, game_id):
+    return render(request, 'games/game.html')
