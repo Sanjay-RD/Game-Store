07db7f995e922e5b33dd59c0af623e52b8248f42
Sanjay-RD <rishidevsanjay9@gmail.com>
2021-07-05 15:35:03 +0545

    user Registration

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib import messages
+from django.contrib.auth.models import User
 # Create your views here.
 
 def dashboard(request):
@@ -17,8 +18,28 @@ def login(request):
 
 def register(request):
     if request.method == "POST":
-        print('register')
-        messages.error(request, 'Testing error message')
+        #get form value
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirmPassword = request.POST['confirm-password']
+        # print(username,email,password,confirmPassword)
+        #check if password match
+        if password == confirmPassword:
+            #check email
+            if User.objects.filter(email=email).exists():
+                messages.error(request,'This Email is already in use')
+                return redirect('account')
+            else:
+                #looks good
+                user = User.objects.create_user(username=username,email=email,password=password)
+                user.save()
+                messages.success(request,'You are now register and you can now login')
+                return redirect('account')
+        else:
+            messages.error(request,'Password doesnot match')
+            return redirect('account')
+    else:
         return redirect('account')
 
 
--- app/gamestore/static/script.js
+++ app/gamestore/static/script.js
@@ -39,10 +39,10 @@ register.addEventListener("click", () => {
   Indicator.style.transform = "translateX(100px)";
 });
 
-let message = document.getElementById("message");
-setTimeout(function () {
-  message.style.display = "none";
-}, 3000);
-// setTimeout(function(){
-//   $("#message").fadeOut("slow");
+// let message = document.getElementById("message");
+// setTimeout(function () {
+//   message.style.display = "none";
 // }, 3000);
+setTimeout(function(){
+  $("#message").fadeOut("slow");
+}, 3000);
