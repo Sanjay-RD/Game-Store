1ad01c24d9acbf7f8a6d7a6fc8b31908e2add492
Sanjay-RD <rishidevsanjay9@gmail.com>
2021-07-09 16:19:19 +0545

    Making Blog/Blogs Dynamic

--- app/blogs/views.py
+++ app/blogs/views.py
@@ -1,11 +1,20 @@
 from django.shortcuts import render
-
+from .models import Blog
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 
 def blog(request, blog_id):
-    return render(request, 'blogs/blog.html')
+    blog = get_object_or_404(Blog,pk=blog_id)
+    context={
+        'blog':blog
+    }
+    return render(request, 'blogs/blog.html', context)
 
 
 def blogs(request):
-    return render(request, 'blogs/blogs.html')
+    blogs = Blog.objects.order_by('-published_date')
+    context = {
+        'blogs': blogs
+    }
+    return render(request, 'blogs/blogs.html', context)
