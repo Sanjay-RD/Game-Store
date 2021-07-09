9cefe5b0d15ca00c327015bcf68a08d41c5f05ea
Sanjay-RD <rishidevsanjay9@gmail.com>
2021-07-09 15:49:56 +0545

    Blog model and Customize Blog Display Data

--- app/blogs/admin.py
+++ app/blogs/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Blog
+
 
 # Register your models here.
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('id', 'blogTitle', 'user_id', 'published_date')
+    list_display_links = ('id', 'blogTitle')
+    list_filter = ('user_name',)
+    search_fields = ('blogTitle', 'blogDescription')
+    list_per_page = 20
+
+
+admin.site.register(Blog, BlogAdmin)
--- app/blogs/models.py
+++ app/blogs/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from ckeditor.fields import RichTextField
+from datetime import datetime
 # Create your models here.
+
+
+class Blog(models.Model):
+    user_id = models.IntegerField()
+    user_name = models.CharField(max_length=200)
+    blogImage = models.ImageField(upload_to='photos/%y/%m/%d/')
+    blogTitle = models.CharField(max_length=200)
+    blogDescription = RichTextField()
+    published_date = models.DateTimeField(default=datetime.now)
+
+    def __str__(self):
+        return self.blogTitle
